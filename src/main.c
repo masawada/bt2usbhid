dfa4967e1275b5510d87eecfa760a5f8889c5588
Masayoshi Wada <developer@andantesoftware.com>
2021-02-01 18:55:11 +0900

    parse options

--- src/main.c
+++ src/main.c
@@ -1,14 +1,46 @@
 #include <stdio.h>
+#include <getopt.h>
 #include <stdlib.h>
 
+#define NO_MODE 0
+#define KEYBOARD_MODE 1
+#define MOUSE_MODE 2
+
+int parse_options (int argc, char *argv[]);
 void show_help (char *program_name);
 
 int main (int argc, char *argv[]) {
   char *program_name = argv[0];
 
-  show_help(program_name);
+  switch (parse_options(argc, argv)) {
+    case KEYBOARD_MODE:
+      printf("Keyboard Mode\n");
+      break;
+    case MOUSE_MODE:
+      printf("Mouse Mode\n");
+      break;
+    default:
+      show_help(program_name);
+      break;
+  }
 
   return 0;
+}
+
+int parse_options (int argc, char *argv[]) {
+  int opt, result = NO_MODE;
+
+  while ((opt = getopt(argc, argv, "mk")) != -1) {
+    switch (opt) {
+      case 'k': result = KEYBOARD_MODE; break;
+      case 'm': result = MOUSE_MODE; break;
+    }
+    if (result != NO_MODE) {
+      break;
+    };
+  }
+
+  return result;
 }
 
 void show_help (char *program_name) {
