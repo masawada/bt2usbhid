2b9d4487b2b175da6161e6545032edf11f5791a1
Masayoshi Wada <developer@andantesoftware.com>
2021-02-01 18:45:52 +0900

    show help

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main () {
-  printf("Hello, World!\n");
+void show_help (char *program_name);
+
+int main (int argc, char *argv[]) {
+  char *program_name = argv[0];
+
+  show_help(program_name);
 
   return 0;
+}
+
+void show_help (char *program_name) {
+  printf("Usage: %s [OPTION]... < /dev/input/eventN > /dev/hidgN\n", program_name);
+  printf("bt2usbhid converts Bluetooth keyboard or mouse events to USB HID reports.");
+  printf("\n");
+  printf("\n");
+  printf("OPTIONS\n");
+  printf("  -k Keyboard mode\n");
+  printf("  -m Mouse mode\n");
+  exit(EXIT_SUCCESS);
 }
